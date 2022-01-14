66ba98f373809d47c3798a2ef76582e3684cb5fc
DioneDev <https://github.com/DioneDev>
2022-01-13 23:50:01 -0300

    Componente MovieCard e cards responsivos conforme tamanho da tela

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,4 +1,4 @@
-import MovieScore from "../../components/MovieScore";
+import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/Pagination";
 
 function Listing(){
@@ -6,7 +6,42 @@ function Listing(){
         // Usando Fragment <> </> porque a funcao aceita apenas 1 componemte
         <> 
             <Pagination />
-            <MovieScore />
+            
+            <div className="container">
+                <div className="row">
+                    
+                    {/* === Divisao grid system do bootstrap ===
+                        sm < 576px 12 col de 12 ou 1 card por linha
+                        sm > 576px 6 col de 12 ou 2 card por linha
+                        lg > 992px 4 col de 12 ou 3 card por linha
+                        xl > 1200px 3 col de 12 ou 4 card por linha
+                    */}
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3"> 
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+                </div>
+            </div>
         </>
     );
 }
